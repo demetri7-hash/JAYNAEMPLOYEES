e634b716919347755d59b2598e56de0127430e62
demetri7-hash <demetri7@gmail.com>
2025-09-03 17:49:13 -0700

    Fix: Allow iframe embedding for Google Sites

--- app/web/next.config.js
+++ app/web/next.config.js
@@ -1,5 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors *;",
+          },
+        ],
+      },
+    ];
+  },
 }
 module.exports = nextConfig
